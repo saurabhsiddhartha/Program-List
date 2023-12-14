0a28590f4bc755419e294f91c26b81a622946478
siddahartha82-creator <SAURABHSIDDHARTHA82@GMAIL.COM>
2023-12-14 23:14:31 +0530

    Remove Duplicate Element in the linked lsit

---
+++ Remove_Duplicate.java
@@ -0,0 +1,63 @@
+ 
+public class Remove_Duplicate {
+	public static Node head;
+	public static Node tail;
+	public static int size;
+	class Node{
+		int data;
+		Node next;
+	 
+	Node(int data){
+		this.data = data;
+		this.next =null;
+	}
+	}
+	public void addlast(int data) {
+		Node newnode = new Node(data);
+		size++; 
+		if(head==null) {
+			head=tail=newnode;
+		}
+		else {
+			tail.next =newnode;
+			tail =newnode;
+		}
+	}
+	public void duplicate() {
+		Node current = head;
+		while(current.next!=null) {
+			if(current.data == current.next.data) {
+				current.next =current.next.next;
+				size--;
+			}
+			else {
+				current =current.next; 
+			 }
+		}
+		tail = current;
+		tail.next =null;
+	}
+	public void print() {
+		Node temp = head;
+		 while(temp!=null){
+			System.out.print(temp.data + "  ");
+			temp = temp.next;
+		} 
+		System.out.println(  " End "); 
+	} 
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Remove_Duplicate list = new Remove_Duplicate();
+		list.addlast(3);
+		list.addlast(3);
+		list.addlast(3);
+		list.addlast(5);
+		list.addlast(4);
+		list.addlast(6);
+		list.print();
+		list.duplicate();
+		list.print();
+	}
+
+}
