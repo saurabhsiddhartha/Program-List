4b43a5ca0557d13ebf49c8e2271420b51c7ba51c
siddahartha82-creator <SAURABHSIDDHARTHA82@GMAIL.COM>
2023-12-17 00:20:29 +0530

    Sorting Algorithm

---
+++ Insertion_Sort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+public class Insertion_Sort {
+     
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6}; 
+        System.out.println("Original array:");
+        System.out.println(Arrays.toString(arr));
+
+        insertionSort(arr); 
+        System.out.println("\nSorted array:"); 
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1; 
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    } 
+}
+
