d37c65a5fbcd5cb3e8262f37cab4962cee6cf6dd
saurabhsiddhartha <153455160+saurabhsiddhartha@users.noreply.github.com>
2023-12-24 13:54:59 +0530

    Update Bubble_sort.java

--- Bubble_sort.java
+++ Bubble_sort.java
@@ -1,28 +1,16 @@
 import java.util.*;
 
 public class Bubble_sort {
-
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        boolean swapped;
-
-        for (int i = 0; i < n - 1; i++) {
-            swapped = false;
-
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // Swap arr[j] and arr[j+1]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-
-                    swapped = true;
+    
+	public static void bubbleSort(int[] arr) {
+        for(int i = 0; i<arr.length;i++){
+            for(int j = 0; j<arr.length-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                   int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
-
-            // If no two elements were swapped by inner loop, the array is sorted
-            if (!swapped)
-                break;
         }
     }
 
